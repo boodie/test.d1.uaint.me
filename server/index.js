11b81b940134be6a9bfd03f66868f14d4d84fa47
Steve LaVigne / A Dimension Beyond, Inc <salbiedermann@gmail.com>
2016-04-15 16:01:02 -0700

    update message

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res) {
-   res.send('hello dude'); 
+   res.send('Good afternoon Steve!'); 
 });
 
 app.listen(5000);
